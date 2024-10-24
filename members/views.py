6a4a4dcb0357deecae248aac50276446b78466f1
BHAVANI-L <bhavanilr8@gmail.com>
2024-10-24 20:22:21 +0530

    Add files via upload

---
+++ members/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.views import generic
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+
+# Create your views here.
+class UserRegisterView(generic.CreateView):
+    form_class=SignUpForm
+    template_name='registration/register.html'
+    success_url=reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    # form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+    # success_url = reverse_lazy('home')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+    
+
+# class EditProfilePageView(generic.UpdateView):
+#     model = Profile
+#     template_name = 'registration/edit_profile_page.html'
+#     # fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url']
+#     success_url = reverse_lazy('home')
